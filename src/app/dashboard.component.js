4968280bee84e4ab063e0634f162f8cbd5b6de40
mdb <mdb@protonmail.ch>
2017-04-23 21:49:17 +0800

    Finished tutorial

--- src/app/dashboard.component.js
+++ src/app/dashboard.component.js
@@ -8,8 +8,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require("@angular/core");
 var hero_service_1 = require("./hero.service");
+var core_1 = require("@angular/core");
 var DashboardComponent = (function () {
     function DashboardComponent(heroService) {
         this.heroService = heroService;
--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-
 
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
